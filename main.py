f129388fd50f6ee4a630568135c0427f1d39936d
sebastianwidmann <swidmann>
2020-04-26 21:21:49 +0200

    main.py added

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+	AE3200 Design Synthesis Exercise
+	Group 09 - Autonomous Environmental Sensing
+
+	Authors: Balsingh Viranca, Boerboom Luc, Cook Jack, Gavra Vlad
+			 Lothaller Phillipe, Park ChangKyu, Pietersma Rolijne,
+			 Robertson Max, Schoenmaker Natascha, Widmann Sebastian
+	Created: 26.04.2020
+	
+	Project Supervisors:
+		- Dr. Irene C. Dedoussi
+		- Dr. Ir. Mirjam Snellen
+		- Ir. Lorenzo Pasqualetto Cassinis
+		- Mark Schelbergen	
+'''
+
