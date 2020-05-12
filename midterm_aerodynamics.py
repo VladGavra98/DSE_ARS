98beb2587c6bb7f8416d1d08383ce3629bdf6ab6
sebastianwidmann <swidmann>
2020-05-12 15:56:23 +0200

    Aerodynamic sizing of concept selection

---
+++ midterm_aerodynamics.py
@@ -0,0 +1,16 @@
+'''
+	AE3200 Design Synthesis Exercise
+	Group 09 - Autonomous Environmental Sensing
+
+	Authors: Park ChangKyu, Widmann Sebastian
+	Created: 12.05.2020
+	
+	Project Supervisors:
+		- Dr. Irene C. Dedoussi
+		- Dr. Ir. Mirjam Snellen
+		- Ir. Lorenzo Pasqualetto Cassinis
+		- Mark Schelbergen
+
+	This script is used for the initial sizing of the propellers / aerodynamic surfaces for the concept selection		
+'''
+
