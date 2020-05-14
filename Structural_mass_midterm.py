a8c42f198b3ae7c50c2fd9bfb9b96bf8aa525a91
Rolijne <47812774+Rolijne@users.noreply.github.com>
2020-05-14 14:22:17 +0200

    Update Structural_mass_midterm.py

--- Structural_mass_midterm.py
+++ Structural_mass_midterm.py
@@ -1,5 +1,6 @@
 """Imports"""
 from math import pi
+from decimal import *
 
 
 
@@ -14,7 +15,7 @@ material_list = ["Average","Tedlar","Zylon Polyurethane and Tedlar", "Vectran an
 m_struc_air = []
 
 for i in range(0,len(ro_hull_list)):
-    m_struc = ro_hull_list[i]*S_air
+    m_struc = round(ro_hull_list[i]*S_air,4)
     m_struc_air.append(m_struc)
     #print("M_struc_air for is", m_struc_air[i], "kg considering", material_list[i])
     
@@ -24,17 +25,21 @@ r_prop = 0.4 #[m]
 r_extra = 0.05 #[m]
 w_arm = 0.04 #[m]
 t_arm = 0.004 #[mm]
-A_extra = 0.25*0.25 #[m2]
-amount_rot = 4
+A_extra = 0.2*0.2 #[m2]
+amount_rot = [3,4,5,6]
 
 r_arm  = r_prop+ r_extra
-V_frame = amount_rot*r_arm*w_arm*t_arm + A_extra*t_arm#[m3]
+
 
 material_drone = ["Aluminium Alloy 6060", "Aramid", "Carbon Fiber"]
 density_drone = [2700, 1400, 1700]
 m_struc_drone = []
 
 for i in range(0, len(material_drone)):
-        m_struc= density_drone[i]*V_frame
-        m_struc_drone.append(m_struc)
-        print("M_struc_drone for is", m_struc_drone[i], "kg considering", material_drone[i])
+    for j in range(0, len(amount_rot)):
+        V_frame = amount_rot[j]*r_arm*w_arm*t_arm + A_extra*t_arm  #[m3]
+        m_struc= round(density_drone[i]*V_frame,4)                      #[kg]
+        m_struc_drone.append(m_struc)                           
+        #print("M_struc_drone for is", m_struc_drone[-1], "kg considering", material_drone[i], "and", amount_rot[j], "rotors")
+
+#Structural Mass VTOL
