bbb3084e583cb63a3fc20e258f6de0b0dbea5cdf
Rolijne <47812774+Rolijne@users.noreply.github.com>
2020-05-14 23:59:25 +0200

    Update Structural_mass_midterm.py

--- Structural_mass_midterm.py
+++ Structural_mass_midterm.py
@@ -1,6 +1,9 @@
 """Imports"""
+import math
 from math import pi
 from decimal import *
+import numpy as np
+from itertools import product
 
 
 
@@ -17,7 +20,9 @@ m_struc_air = []
 for i in range(0,len(ro_hull_list)):
     m_struc = round(ro_hull_list[i]*S_air,4)
     m_struc_air.append(m_struc)
-    #print("M_struc_air for is", m_struc_air[i], "kg considering", material_list[i])
+    print("M_struc_air for is", m_struc_air[i], "kg considering", material_list[i])
+
+#print(np.average(m_struc_air))
     
 
 #Structural Mass Drone Midterm
@@ -26,7 +31,7 @@ r_extra = 0.05 #[m]
 w_arm = 0.04 #[m]
 t_arm = 0.004 #[mm]
 A_extra = 0.2*0.2 #[m2]
-amount_rot = [3,4,5,6]
+amount_rot = [8]
 
 r_arm  = r_prop+ r_extra
 
@@ -40,6 +45,35 @@ for i in range(0, len(material_drone)):
         V_frame = amount_rot[j]*r_arm*w_arm*t_arm + A_extra*t_arm  #[m3]
         m_struc= round(density_drone[i]*V_frame,4)                      #[kg]
         m_struc_drone.append(m_struc)                           
-        #print("M_struc_drone for is", m_struc_drone[-1], "kg considering", material_drone[i], "and", amount_rot[j], "rotors")
-
+        print("M_struc_drone for is", m_struc_drone[-1], "kg considering", material_drone[i], "and", amount_rot[j], "rotors")
+print(V_frame*1000000)
 #Structural Mass VTOL
+S_w = np.arange(21.527821 ,43.055642, 1)                     #Wingarea [ft2]
+W_fw = 3.840/0.453592                                        #weight fuel wing [lb]
+A = np.arange(5,10,0.05)                                     #Aspect ratio [-]
+taper = np.arange(0.2, 0.5, 0.01)                            #taper [-]
+q = 2222#1993.8                                                   #dynamic pressure at cruise [lb/ft2]
+tc = np.arange(0.1, 0.2, 0.005)                               #thickness over chord [-]
+Nz = 1.5*1.5                                                 # loadfactor [-]
+Wdg = 12.07/0.453592                                         #design gross weight [lb]
+
+
+W_wing_list = []
+
+#for s in S_w:
+#    for a in A:
+#        for t in taper:
+#                for c in tc:
+#                        r1 = 0.036*s**0.758
+#                        r2 = W_fw**0.0035
+#                        r3 = (a/(math.cos(0))**2)**0.6
+#                        r4 = q**0.006
+#                        r5 = t**0.04
+#                        r6 = (100*c/(math.cos(0)))**(-0.3)
+#                        r7 = (Nz*Wdg)**0.59
+#                        W_wing = r1*r2*r3*r4*r5*r6*r7*0.453592 #[kg]
+#                        W_wing_list.append(W_wing)
+#                       
+#print(np.amin(W_wing_list),np.amax(W_wing_list))
+#print(np.average(W_wing_list))
+
