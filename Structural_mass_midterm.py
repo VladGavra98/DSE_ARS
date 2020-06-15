283fc3a7623bdad2679ef896c6a4648e0e7e973a
Rolijne <47812774+Rolijne@users.noreply.github.com>
2020-06-15 12:21:18 +0200

    CATIA Files for back-up

--- Structural_mass_midterm.py
+++ Structural_mass_midterm.py
@@ -9,9 +9,10 @@ from itertools import product
 
 #Structural Mass Airship Midterm
 V_air = 229.85      #[m3]
-r_air = 3.8         #[m]
+r_air = 2.6         #[m]
+cylinder = 7.8
 
-S_air = 4*pi*r_air*r_air    #[m2]
+S_air = 4*pi*r_air*r_air  + 2*pi*r_air*cylinder  #[m2]
 
 ro_hull_list = [0.224653, 0.074, 0.18, 0.196, 0.208, 0.17] #[kg/m2]
 material_list = ["Average","Tedlar","Zylon Polyurethane and Tedlar", "Vectran and Polyethane", "Zylon Polyurethane", "Vectran"]
@@ -20,20 +21,21 @@ m_struc_air = []
 for i in range(0,len(ro_hull_list)):
     m_struc = round(ro_hull_list[i]*S_air,4)
     m_struc_air.append(m_struc)
-    print("M_struc_air for is", m_struc_air[i], "kg considering", material_list[i])
+    #print("M_struc_air for is", m_struc_air[i], "kg considering", material_list[i])
+#print(S_air)
 
-#print(np.average(m_struc_air))
+
     
 
 #Structural Mass Drone Midterm
 r_prop = 0.4 #[m]
-r_extra = 0.05 #[m]
-w_arm = 0.04 #[m]
-t_arm = 0.004 #[mm]
+r_extra = 0 #[m]
+w_arm = 0.05 #[m]
+t_arm = 0.003 #[m]
 A_extra = 0.2*0.2 #[m2]
-amount_rot = [8]
+amount_rot = [4]
 
-r_arm  = r_prop+ r_extra
+r_arm  = r_prop + r_extra
 
 
 material_drone = ["Aluminium Alloy 6060", "Aramid", "Carbon Fiber"]
@@ -42,38 +44,41 @@ m_struc_drone = []
 
 for i in range(0, len(material_drone)):
     for j in range(0, len(amount_rot)):
-        V_frame = amount_rot[j]*r_arm*w_arm*t_arm + A_extra*t_arm  #[m3]
+        V_frame = amount_rot[j]*r_arm*0.5*2*pi*t_arm*w_arm + A_extra*0.004  #[m3]
         m_struc= round(density_drone[i]*V_frame,4)                      #[kg]
         m_struc_drone.append(m_struc)                           
         print("M_struc_drone for is", m_struc_drone[-1], "kg considering", material_drone[i], "and", amount_rot[j], "rotors")
-print(V_frame*1000000)
+print(V_frame)
+
 #Structural Mass VTOL
-S_w = np.arange(21.527821 ,43.055642, 1)                     #Wingarea [ft2]
-W_fw = 3.840/0.453592                                        #weight fuel wing [lb]
-A = np.arange(5,10,0.05)                                     #Aspect ratio [-]
-taper = np.arange(0.2, 0.5, 0.01)                            #taper [-]
-q = 2222#1993.8                                                   #dynamic pressure at cruise [lb/ft2]
-tc = np.arange(0.1, 0.2, 0.005)                               #thickness over chord [-]
-Nz = 1.5*1.5                                                 # loadfactor [-]
-Wdg = 12.07/0.453592                                         #design gross weight [lb]
-
-
-W_wing_list = []
-
+#S_w = np.arange(5.38196 ,16.1459, 0.5) #Wingarea [ft2]
+#W_fw = 0.564/0.453592                                        #weight fuel wing [lb]
+#A = np.arange(5,10,0.05)
+#sweep = np.arange(0.523599, 0.872665,0.1)                        #Aspect ratio [-]
+#taper = np.arange(0.2, 0.5, 0.01)                            #taper [-]
+#q = 11.513                                                   #dynamic pressure at cruise [lb/ft2]
+#tc = np.arange(0.1, 0.2, 0.005)                              #thickness over chord [-]
+#Nz = 1.5*3                                                   # loadfactor [-]
+#Wdg = 12.07/0.453592                                         #design gross weight [lb]
+#
+#
+#W_wing_list = []
+#
 #for s in S_w:
 #    for a in A:
-#        for t in taper:
-#                for c in tc:
-#                        r1 = 0.036*s**0.758
-#                        r2 = W_fw**0.0035
-#                        r3 = (a/(math.cos(0))**2)**0.6
-#                        r4 = q**0.006
-#                        r5 = t**0.04
-#                        r6 = (100*c/(math.cos(0)))**(-0.3)
-#                        r7 = (Nz*Wdg)**0.59
-#                        W_wing = r1*r2*r3*r4*r5*r6*r7*0.453592 #[kg]
-#                        W_wing_list.append(W_wing)
+#        for l in sweep:
+#            for t in taper:
+#                    for c in tc:
+#                            r1 = 0.036*s**0.758
+#                            r2 = W_fw**0.0035
+#                            rx = (math.cos(l))**2
+#                            r3 = (a/rx)**0.6
+#                            r4 = q**(0.006*t**0.04)
+#                            r5 = 1
+#                            r6 = (100*c/(math.cos(l)))**(-0.3)
+#                            r7 = (Nz*Wdg)**0.49
+#                            W_wing = r1*r2*r3*r4*r5*r6*r7*0.453592 #[kg]
+#                            W_wing_list.append(W_wing)
 #                       
 #print(np.amin(W_wing_list),np.amax(W_wing_list))
 #print(np.average(W_wing_list))
-
